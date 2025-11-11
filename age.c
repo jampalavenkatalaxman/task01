d2b7fbef1f5a5d5ed70a6b2d9625bd8fd23ab0e9
jampalavenkatalaxman <jampalavenkatalaxman123@gmail.com>
2025-11-11 15:40:05 +0530

    task3

--- age.c
+++ age.c
@@ -5,10 +5,8 @@ int main() {
     printf("Enter your age: ");
     scanf("%d", &age);
 
-    if (age >= 18)
+    if (age = 18)
         printf("You are eligible to vote.\n");
-    else
-        printf("You are not eligible to vote.\n");
 
     return 0;
 }
